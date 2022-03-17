172fac7bd67950d00ba212ef14a5c8d4def775bf
salman <salmannova@gmail.com>
2022-03-17 19:53:23 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,109 @@
-import Confetti from 'react-confetti'
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import {
+  Grid
+} from "@giphy/react-components";
+import React, { useState, useEffect } from "react";
+import Confetti from 'react-confetti';
+import ReactPlayer from 'react-player';
+import ResizeObserver from "react-resize-observer";
+import styled, { keyframes } from 'styled-components';
+import ReactAudioPlayer from 'react-audio-player';
 
-import logo from './logo.svg';
 import './App.css';
 
+
+const giphyFetch = new GiphyFetch("4bw2PKsNGPKk281WZnGHKLnJpQejW5bP");
+
+const GridDemo = ({ onGifClick }) => {
+  const fetchGifs = (offset: number) =>
+    giphyFetch.search("birthday", { offset, limit: 10 });
+  const [width, setWidth] = useState(window.innerWidth);
+  return (
+    <>
+      <Grid
+        onGifClick={onGifClick}
+        fetchGifs={fetchGifs}
+        width={width}
+        columns={3}
+        gutter={6}
+      />
+      <ResizeObserver
+        onResize={({ width }) => {
+          setWidth(width);
+        }}
+      />
+    </>
+  );
+}
+
 const App = () => {
+  const [modalGif, setModalGif] = useState();
+  useEffect(() => {
+    if (document) {
+      var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+      if (!isChrome) {
+        const elem = document.getElementById('iframeAudo')
+        if (elem) {
+          elem.remove()
+        }
+      }
+      else {
+        const elem = document.getElementById('playAudio')
+        if (elem) {
+          elem.remove()
+        }
+      }
+    }
+
+  }, [])
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Confetti />
+
+      <Confetti style={{ position: 'fixed' }} />
+      <GridDemo
+        onGifClick={(gif, e) => {
+          console.log("gif", gif);
+          e.preventDefault();
+          setModalGif(gif);
+        }}
+      />
+      <Div>
+        <ReactPlayer url="/video.mov" />
+        <ReactAudioPlayer
+          src="/song.mp3#t=00:01:13"
+          autoPlay
+          controls
+        />
+        <audio id="player" controls autoPlay={true}>
+          <source src="/song.mp3#t=00:01:13" type="audio/mpeg" />
+        </audio>
+        <iframe title="frame" src="/song.mp3#t=00:01:13" allow="autoplay" style={{ display: 'none' }} id="iframeAudio">
+        </iframe>
+
+      </Div>
     </div>
   );
 }
 
 export default App;
+
+const pulse = keyframes`
+  0% { scale: 1 }
+ 30% { scale:1.1 }
+ 40% { scale:1.2 }
+ 100% { scale:1 }
+
+`
+
+const Div = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+  animation-name: ${pulse};
+  animation-duration: 8s;
+  animation-iteration-count: infinite;
+`;
