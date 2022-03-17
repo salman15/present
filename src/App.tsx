d2c7f1d4b6e86c3ef225c827845b01237cadc731
salman <salmannova@gmail.com>
2022-03-17 21:11:54 +0100

    updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import ReactPlayer from 'react-player';
 import ResizeObserver from "react-resize-observer";
 import styled, { keyframes } from 'styled-components';
 import ReactAudioPlayer from 'react-audio-player';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
 
@@ -38,6 +40,7 @@ const GridDemo = ({ onGifClick }) => {
 
 const App = () => {
   const [modalGif, setModalGif] = useState();
+  const [play, setPlay] = useState(false)
   useEffect(() => {
     if (document) {
       var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
@@ -57,6 +60,18 @@ const App = () => {
 
   }, [])
 
+  const playAudo = () => {
+    const audio = document.getElementById("audio") as HTMLAudioElement
+    if (play) {
+      audio.pause();
+      setPlay(false)
+    } else {
+      setPlay(true)
+      audio.play();
+
+    }
+  }
+
 
   return (
     <div className="App">
@@ -71,14 +86,16 @@ const App = () => {
       />
       <Div>
         <ReactPlayer url="/video.mov" />
+        <PlayButton onClick={playAudo}><FontAwesomeIcon icon={faPlay} color="black" size="8x" style={{ opacity: play ? 0 : 0.4 }} /></PlayButton>
         <ReactAudioPlayer
+          id="audio"
           src="/song.mp3#t=00:01:13"
           autoPlay
           controls
+          style={{
+            display: 'none'
+          }}
         />
-
-
-
       </Div>
     </div>
   );
@@ -94,8 +111,23 @@ const pulse = keyframes`
 
 `
 
+const PlayButton = styled.button`
+  border: none;
+  padding: 24;
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  border-radius: 50%;
+  background-color: transparent;
+
+`;
+
 const Div = styled.div`
   position: fixed;
+  padding: 32px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -103,4 +135,20 @@ const Div = styled.div`
   animation-name: ${pulse};
   animation-duration: 8s;
   animation-iteration-count: infinite;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  div {
+    width: 80% !important;
+    height: 80% !important;
+    min-width: 200px;
+    min-height: 200px;
+    border-radius: 50%;
+    overflow: hidden;
+    box-shadow: 5px 10px 18px #888888;
+
+  }
 `;
